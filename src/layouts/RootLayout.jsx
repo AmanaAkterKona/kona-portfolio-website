6499298c9c763638d0400491b5aaf7db7a40aafe
AmanaAkterKona <Proff.kona@gmail.com>
2026-04-26 19:48:16 +0600

    remove costome coursor

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../pages/Shared/Navbar/Navbar';
 import Footer from '../pages/Shared/Footer/Footer';
-import CustomCursor from '../pages/CustomCursor';
+//import CustomCursor from '../pages/CustomCursor';
 import About from '../pages/About';
 import Skills from '../pages/Skills';
 import Projects from '../pages/Projects';
@@ -11,7 +11,7 @@ import Contact from '../pages/Contact';
 const RootLayout = () => {
   return (
     <div className="relative bg-[#060e22]"> {/* এখানে bg যোগ করুন */}
-      <CustomCursor />
+      
       <Navbar /> 
 
       <main>
